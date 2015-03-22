6865425b5b2360ea37867a2a85f4cbe16162016a
Cameron Bates <darksonic8000@gmail.com>
2015-03-22 17:09:53 -0400

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+angular.module('nerdyfm', ['ionic', 'nerdyfm.controller', 'ngCordova'])
+
+.run(function($ionicPlatform, $cordovaStatusbar, $interval) {
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+
+        //Set the statusbar color for iOS
+        try {
+            $cordovaStatusbar.overlaysWebView(true).style(1);
+        } catch (e) {
+            // console.log(e);
+        }
+    });
+});
